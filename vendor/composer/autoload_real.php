8418c0bb503a5870643a6d648f9cf727ba18cfcf
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2025-08-24 16:35:11 +0200

    composer dump-autoload -o

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -4,35 +4,35 @@
 
 class ComposerAutoloaderInitdb0506f73b171ab12c3437ae07e258c1
 {
-  private static $loader;
+    private static $loader;
 
-  public static function loadClassLoader($class)
-  {
-    if ('Composer\Autoload\ClassLoader' === $class) {
-      require __DIR__ . '/ClassLoader.php';
-    }
-  }
-
-  /**
-   * @return \Composer\Autoload\ClassLoader
-   */
-  public static function getLoader()
-  {
-    if (null !== self::$loader) {
-      return self::$loader;
+    public static function loadClassLoader($class)
+    {
+        if ('Composer\Autoload\ClassLoader' === $class) {
+            require __DIR__ . '/ClassLoader.php';
+        }
     }
 
-    require __DIR__ . '/platform_check.php';
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
+    public static function getLoader()
+    {
+        if (null !== self::$loader) {
+            return self::$loader;
+        }
 
-    spl_autoload_register(array('ComposerAutoloaderInitdb0506f73b171ab12c3437ae07e258c1', 'loadClassLoader'), true, true);
-    self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
-    spl_autoload_unregister(array('ComposerAutoloaderInitdb0506f73b171ab12c3437ae07e258c1', 'loadClassLoader'));
+        require __DIR__ . '/platform_check.php';
 
-    require __DIR__ . '/autoload_static.php';
-    call_user_func(\Composer\Autoload\ComposerStaticInitdb0506f73b171ab12c3437ae07e258c1::getInitializer($loader));
+        spl_autoload_register(array('ComposerAutoloaderInitdb0506f73b171ab12c3437ae07e258c1', 'loadClassLoader'), true, true);
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        spl_autoload_unregister(array('ComposerAutoloaderInitdb0506f73b171ab12c3437ae07e258c1', 'loadClassLoader'));
 
-    $loader->register(true);
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInitdb0506f73b171ab12c3437ae07e258c1::getInitializer($loader));
 
-    return $loader;
-  }
+        $loader->register(true);
+
+        return $loader;
+    }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,32 +6,23 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitdb0506f73b171ab12c3437ae07e258c1
 {
-  public static $classMap = array(
-    'APP\\plugins\\generic\\pidManager\\classes\\Helpers\\PID\\AbstractPid' => __DIR__ . '/../..' . '/classes/Helpers/PID/AbstractPid.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Helpers\\PID\\Doi' => __DIR__ . '/../..' . '/classes/Helpers/PID/Doi.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\Igsn' => __DIR__ . '/../..' . '/classes/Igsn/Igsn.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnArticleDetails' => __DIR__ . '/../..' . '/classes/Igsn/IgsnArticleDetails.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnConstants' => __DIR__ . '/../..' . '/classes/Igsn/IgsnConstants.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnDAO' => __DIR__ . '/../..' . '/classes/Igsn/IgsnDAO.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnDataModel' => __DIR__ . '/../..' . '/classes/Igsn/IgsnDataModel.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnForm' => __DIR__ . '/../..' . '/classes/Igsn/IgsnForm.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnPublicationTab' => __DIR__ . '/../..' . '/classes/Igsn/IgsnPublicationTab.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnRepo' => __DIR__ . '/../..' . '/classes/Igsn/IgsnRepo.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnSchema' => __DIR__ . '/../..' . '/classes/Igsn/IgsnSchema.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnSchemaMigration' => __DIR__ . '/../..' . '/classes/Igsn/IgsnSchemaMigration.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnSubmissionWizard' => __DIR__ . '/../..' . '/classes/Igsn/IgsnSubmissionWizard.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Pidinst\\PidinstConstants' => __DIR__ . '/../..' . '/classes/Pidinst/PidinstConstants.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Pidinst\\PidinstSchema' => __DIR__ . '/../..' . '/classes/Pidinst/PidinstSchema.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Settings\\Actions' => __DIR__ . '/../..' . '/classes/Settings/Actions.php',
-    'APP\\plugins\\generic\\pidManager\\classes\\Settings\\Manage' => __DIR__ . '/../..' . '/classes/Settings/Manage.php',
-    'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-  );
+    public static $classMap = array (
+        'APP\\plugins\\generic\\pidManager\\classes\\Constants' => __DIR__ . '/../..' . '/classes/Constants.php',
+        'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnArticleDetails' => __DIR__ . '/../..' . '/classes/Igsn/IgsnArticleDetails.php',
+        'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnDataModel' => __DIR__ . '/../..' . '/classes/Igsn/IgsnDataModel.php',
+        'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnForm' => __DIR__ . '/../..' . '/classes/Igsn/IgsnForm.php',
+        'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnPublicationTab' => __DIR__ . '/../..' . '/classes/Igsn/IgsnPublicationTab.php',
+        'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnRepo' => __DIR__ . '/../..' . '/classes/Igsn/IgsnRepo.php',
+        'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnSchema' => __DIR__ . '/../..' . '/classes/Igsn/IgsnSchema.php',
+        'APP\\plugins\\generic\\pidManager\\classes\\Igsn\\IgsnSubmissionWizard' => __DIR__ . '/../..' . '/classes/Igsn/IgsnSubmissionWizard.php',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
 
-  public static function getInitializer(ClassLoader $loader)
-  {
-    return \Closure::bind(function () use ($loader) {
-      $loader->classMap = ComposerStaticInitdb0506f73b171ab12c3437ae07e258c1::$classMap;
+    public static function getInitializer(ClassLoader $loader)
+    {
+        return \Closure::bind(function () use ($loader) {
+            $loader->classMap = ComposerStaticInitdb0506f73b171ab12c3437ae07e258c1::$classMap;
 
-    }, null, ClassLoader::class);
-  }
+        }, null, ClassLoader::class);
+    }
 }
