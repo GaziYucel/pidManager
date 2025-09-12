1edc46ede0eab47674449b17540aa967e21d3d93
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2025-09-12 22:00:39 +0200

    Cleanup; case sensitive fix

--- PidManagerPlugin.php
+++ PidManagerPlugin.php
@@ -21,12 +21,10 @@ use APP\plugins\generic\pidManager\classes\Constants;
 use APP\plugins\generic\pidManager\classes\Settings\Actions;
 use APP\plugins\generic\pidManager\classes\Settings\Manage;
 use APP\plugins\generic\pidManager\classes\Igsn\ArticleDetails as IgsnArticleDetails;
-use APP\plugins\generic\pidManager\classes\Igsn\DataModel as IgsnDataModel;
 use APP\plugins\generic\pidManager\classes\Igsn\Schema as IgsnSchema;
 use APP\plugins\generic\pidManager\classes\Igsn\SubmissionWizard as IgsnSubmissionWizard;
 use APP\plugins\generic\pidManager\classes\Igsn\Workflow as IgsnWorkflow;
 use APP\plugins\generic\pidManager\classes\Pidinst\ArticleDetails as PidinstArticleDetails;
-use APP\plugins\generic\pidManager\classes\Pidinst\DataModel as PidinstDataModel;
 use APP\plugins\generic\pidManager\classes\Pidinst\Schema as PidinstSchema;
 use APP\plugins\generic\pidManager\classes\Pidinst\SubmissionWizard as PidinstSubmissionWizard;
 use APP\plugins\generic\pidManager\classes\Pidinst\Workflow as PidinstWorkflow;
@@ -48,10 +46,10 @@ class PidManagerPlugin extends GenericPlugin
 
                 /** IGSN */
                 if ($this->getSetting($contextId, Constants::settingEnableIgsn)) {
-                    $igsnSchema = new IgsnSchema(Constants::igsn);
-                    $igsnWorkflow = new IgsnWorkflow($this, Constants::igsn, new IgsnDataModel());
-                    $igsnArticleDetails = new IgsnArticleDetails($this, Constants::igsn, new IgsnDataModel());
-                    $igsnSubmissionWizard = new IgsnSubmissionWizard($this, Constants::igsn);
+                    $igsnSchema = new IgsnSchema();
+                    $igsnWorkflow = new IgsnWorkflow($this);
+                    $igsnArticleDetails = new IgsnArticleDetails($this);
+                    $igsnSubmissionWizard = new IgsnSubmissionWizard($this);
 
                     Hook::add('Schema::get::publication', [$igsnSchema, 'addToSchemaPublication']);
                     Hook::add('Template::Workflow::Publication', [$igsnWorkflow, 'execute']);
@@ -63,10 +61,10 @@ class PidManagerPlugin extends GenericPlugin
 
                 /** PIDINST */
                 if ($this->getSetting($contextId, Constants::settingEnablePidinst)) {
-                    $pidinstSchema = new PidinstSchema(Constants::pidinst);
-                    $pidinstWorkflow = new PidinstWorkflow($this, Constants::pidinst, new PidinstDataModel());
-                    $pidinstArticleDetails = new PidinstArticleDetails($this, Constants::pidinst, new PidinstDataModel());
-                    $pidinstSubmissionWizard = new PidinstSubmissionWizard($this, Constants::pidinst);
+                    $pidinstSchema = new PidinstSchema();
+                    $pidinstWorkflow = new PidinstWorkflow($this,);
+                    $pidinstArticleDetails = new PidinstArticleDetails($this);
+                    $pidinstSubmissionWizard = new PidinstSubmissionWizard($this);
 
                     Hook::add('Schema::get::publication', [$pidinstSchema, 'addToSchemaPublication']);
                     Hook::add('Template::Workflow::Publication', [$pidinstWorkflow, 'execute']);
@@ -79,8 +77,7 @@ class PidManagerPlugin extends GenericPlugin
                 if ($this->getSetting($contextId, Constants::settingEnableIgsn) ||
                     $this->getSetting($contextId, Constants::settingEnablePidinst)
                 ) {
-                    $this->addStyleSheetToBackend($request, $templateMgr);
-                    $this->addStyleSheetToFrontend($request, $templateMgr);
+                    $this->addStyleSheet($request, $templateMgr);
                 }
             }
             return true;
@@ -114,17 +111,14 @@ class PidManagerPlugin extends GenericPlugin
         return $manage->execute($args, $request);
     }
 
-    protected function addStyleSheetToBackend(Request $request, TemplateManager $templateMgr): void
+    protected function addStyleSheet(Request $request, TemplateManager $templateMgr): void
     {
         $templateMgr->addStyleSheet("pidManagerStylesBackend",
             "{$request->getBaseUrl()}/{$this->getPluginPath()}/assets/css/backend.css", [
                 'inline' => false,
                 'contexts' => ['backend']
             ]);
-    }
 
-    protected function addStyleSheetToFrontend(Request $request, TemplateManager $templateMgr): void
-    {
         $templateMgr->addStyleSheet("pidManagerStylesFrontend",
             "{$request->getBaseUrl()}/{$this->getPluginPath()}/assets/css/frontend.css", [
                 'inline' => false,
