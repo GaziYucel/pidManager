e116e487590033e8e85e7af6a9cfe7d743040a61
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2025-10-24 11:30:58 +0200

    More efficient filter function

--- resources/js/Components/Workflow.vue
+++ resources/js/Components/Workflow.vue
@@ -544,13 +544,14 @@ const setItemsFilterPhrase = (value) => {
 	itemsFilterPhrase.value = value;
 };
 const itemsFiltered = computed(() => {
-	if (itemsFilterPhrase.value) {
-		return items.value.filter((item) =>
-			JSON.stringify(item).toLowerCase().includes(itemsFilterPhrase.value.toLowerCase()),
-		);
-	} else {
-		return items.value;
-	}
+	const data = items.value || [];
+	return itemsFilterPhrase.value
+		? data.filter((item) =>
+				JSON.stringify(item)
+					.toLowerCase()
+					.includes(itemsFilterPhrase.value.toString().toLowerCase()),
+			)
+		: items.value;
 });
 
 /* Items */
